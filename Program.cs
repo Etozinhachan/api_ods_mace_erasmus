45bc514a5aba5031e378139819ca56125bd4e28b
Etozinhachan <etochandev@gmail.com>
2024-09-05 21:26:51 +0100

    a tentar fazer funcionar o dotenv25 DELETAR

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ var config = builder.Configuration;
 
 //Environment.SetEnvironmentVariable("JWT_Key", "rawr");
 
-DotEnv.Load("/etc/secrets/.env");
+DotEnv.Load("etc/secrets/.env");
 
 builder.Services.AddAuthentication(x =>
 {
@@ -63,7 +63,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 //var dbConnectionString = "server=" + Environment.GetEnvironmentVariable("Database_Server") + ";database=" + Environment.GetEnvironmentVariable("Database_Name") + ";user=" + Environment.GetEnvironmentVariable("Database_User") + ";password=" + Environment.GetEnvironmentVariable("Database_Password") + ";";
 
-var dbConnectionString = "server=erasmus-esl.pt; database=erasmus3_api_ods_mace_erasmus; user=erasmus3_api_ods_mace_erasmus; password=hwyE1V2by75z";
+var dbConnectionString = "server=erasmus-esl.pt; database=erasmus3_api_ods_mace_erasmus; user=erasmus3_api_ods_mace_erasmus; password=hwyE1V2by75z; default command timeout=60;";
 
 //var dbConnectionString = config.GetConnectionString("Default");
 
